091f483912646fa8857bcc2c5a413a088232b8d5
Cheung <j.cheung@student.tue.nl>
2024-12-01 22:27:43 +0100

    code improvements

--- chatbot_ui.py
+++ chatbot_ui.py
@@ -1,5 +1,5 @@
 from flask import Flask, request, jsonify, render_template
-from query_data2 import query_rag  # Import your chatbot logic
+from query_data2 import query_rag 
 
 app = Flask(__name__)
 
@@ -10,7 +10,7 @@ def home():
 @app.route("/send_message", methods=["POST"])
 def send_message():
     user_message = request.json.get("message", "")
-    chatbot_response = query_rag(user_message)  # Call your chatbot logic
+    chatbot_response = query_rag(user_message)  
     return jsonify({"response": chatbot_response})
 
 if __name__ == "__main__":
